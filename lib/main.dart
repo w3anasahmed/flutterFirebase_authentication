a478ce22f3a227cb9e255e52419bc6788003fbb8
w3anasahmed <w3anasahmed@gmail.com>
2026-02-05 19:39:56 +0500

    code of firebase auth assign the user through the console.firebase and control flow in app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:authentication/screens/auth_page.dart';
 import 'package:authentication/screens/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -10,6 +16,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: AuthPage());
   }
 }
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,123 +1,140 @@
 import 'package:authentication/components/button.dart';
 import 'package:authentication/components/square_tile.dart';
 import 'package:authentication/components/textfield.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
   //controllers
-  TextEditingController usernameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
   //void sign
-  void userSignIn() {}
+  void userSignIn() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: emailController.text,
+      password: passwordController.text,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade300,
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 70),
-            //logo
-            Icon(Icons.lock, size: 95),
-            SizedBox(height: 50),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 70),
+              //logo
+              Icon(Icons.lock, size: 95),
+              SizedBox(height: 50),
 
-            //welcome back , you've been missed!
-            Text(
-              'Welcome back you\'ve been missed!',
-              style: TextStyle(fontSize: 16, color: Colors.grey.shade700),
-            ),
-            SizedBox(height: 25),
-            //user textfield
-            CustomTextField(
-              controller: usernameController,
-              hintText: 'Username',
-              obscureText: false,
-            ),
-            SizedBox(height: 20),
-            //password textfield
-            CustomTextField(
-              controller: passwordController,
-              hintText: 'Password',
-              obscureText: true,
-            ),
-            SizedBox(height: 10),
-            //forgot password?
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 25.0),
-                  child: Text(
-                    'forgot password?',
-                    style: TextStyle(fontSize: 16, color: Colors.grey.shade600),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 25),
-            //sign in button
-            CustomButton(onPressed: () => userSignIn()),
-            SizedBox(height: 50),
-            //or continue with
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Row(
+              //welcome back , you've been missed!
+              Text(
+                'Welcome back you\'ve been missed!',
+                style: TextStyle(fontSize: 16, color: Colors.grey.shade700),
+              ),
+              SizedBox(height: 25),
+              //user textfield
+              CustomTextField(
+                controller: emailController,
+                hintText: 'Username',
+                obscureText: false,
+              ),
+              SizedBox(height: 20),
+              //password textfield
+              CustomTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+              SizedBox(height: 10),
+              //forgot password?
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Expanded(
-                    child: Divider(thickness: 0.5, color: Colors.grey.shade400),
-                  ),
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    padding: const EdgeInsets.only(right: 25.0),
                     child: Text(
-                      'Or continue with',
+                      'forgot password?',
                       style: TextStyle(
-                        color: Colors.grey.shade700,
-                        fontSize: 18,
+                        fontSize: 16,
+                        color: Colors.grey.shade600,
                       ),
                     ),
                   ),
-                  Expanded(child: Divider(thickness: 0.5, color: Colors.grey)),
                 ],
               ),
-            ),
-            SizedBox(height: 50),
-            //google + apple
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SquareTile(imagePath: "images/google.png"),
-                SizedBox(width: 25),
-                SquareTile(imagePath: 'images/apple.png'),
-              ],
-            ),
-            SizedBox(height: 50),
-            //not a member? register now
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Not a member?',
-                  style: TextStyle(color: Colors.grey.shade700, fontSize: 18),
+              SizedBox(height: 25),
+              //sign in button
+              CustomButton(onPressed: () => userSignIn()),
+              SizedBox(height: 50),
+              //or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey.shade400,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Or continue with',
+                        style: TextStyle(
+                          color: Colors.grey.shade700,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(thickness: 0.5, color: Colors.grey),
+                    ),
+                  ],
                 ),
-                SizedBox(width: 8),
-                Text(
-                  'Register now',
-                  style: TextStyle(
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
+              ),
+              SizedBox(height: 50),
+              //google + apple
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SquareTile(imagePath: "images/google.png"),
+                  SizedBox(width: 25),
+                  SquareTile(imagePath: 'images/apple.png'),
+                ],
+              ),
+              SizedBox(height: 50),
+              //not a member? register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Not a member?',
+                    style: TextStyle(color: Colors.grey.shade700, fontSize: 18),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  SizedBox(width: 8),
+                  Text(
+                    'Register now',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
